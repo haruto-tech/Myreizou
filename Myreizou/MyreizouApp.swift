e6c7e799074294fb48ec60544bcc3491ad18c524
sumi <haruto@sumi.tv>
2026-06-06 17:50:55 +0900

    Initial Commit

---
+++ Myreizou/MyreizouApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyreizouApp.swift
+//  Myreizou
+//
+//  Created by はると on 2026/06/06.
+//
+
+import SwiftUI
+
+@main
+struct MyreizouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
